956faa62a3888f59751a7f057e39e7d089a3e35e
Mustafa Mahmoud <dreadnights5@gmail.com>
2021-02-22 08:21:29 +0200

    doing enhancments, add a checkbox, style it using scss

--- src/toDoApp/Tasks/Task/Task.js
+++ src/toDoApp/Tasks/Task/Task.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './Task.scss';
 function Task(props) {
+	const { id, title, checked } = props; // task properties
 
-    const openModalAndGetTask = () => {
-        // open the modal and get the clicked task
-        props.getSingleTask(props.id)
-        props.openModal();
-    }
+	const openModalAndGetTask = () => {
+		// open the modal and get the clicked task
+		props.getSingleTask(id);
+		props.openModal();
+	};
 
-    return (
-        <div className='task'>
-            <div className='taskText'>{props.title}</div>
-            <div className='taskEdit' onClick = {openModalAndGetTask}>&#9998;</div>
+	return (
+		<div className='task'>
+			<p className={`taskText ${checked}`}>{title}</p>
+			<div className='controls'>
+				<label className='container'>
+					<input type='checkbox' />
+					<span className='checkmark' />
+				</label>
+				<div className='taskEdit' onClick={openModalAndGetTask}>
+					&#9998;
+				</div>
 
-            <div className='taskDelete' onClick={() => props.deleteTask(props.id, props.showTasks)}>&#128465;</div>
-
-        </div>
-    )
+				<div className='taskDelete' onClick={() => props.deleteTask(id, props.showTasks)}>
+					&#128465;
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Task;
--- src/toDoApp/ToDo.js
+++ src/toDoApp/ToDo.js
@@ -1,164 +1,162 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './ToDo.scss';
 import Tasks from './Tasks/Tasks';
 import Modal from '../modal/modal';
 export class ToDo extends Component {
+	state = {
+		task: '',
+		tasks: [],
+		targetTask: {},
+		modalOpened: false
+	};
 
-    state = {
-        task: '',
-        tasks: [],
-        targetTask: {},
-        modalOpened: false
-    }
+	writeTaskHandler = e => {
+		// store user`s task in the state
+		this.setState({ task: e.target.value });
+	};
 
-    writeTaskHandler = (e) => {
-        // store user`s task in the state
-        this.setState({task: e.target.value})
-    }
+	getTasksFromLocalStorage = () => {
+		// if there`re tasks in localStorage, return them, else, return []
 
-    getTasksFromLocalStorage = () => {
-        // if there`re tasks in localStorage, return them, else, return []
+		let tasks = [];
 
-        let tasks = [];
+		if (localStorage.getItem('tasks') !== null) {
+			tasks = JSON.parse(localStorage.getItem('tasks'));
+		}
 
-        if(localStorage.getItem('tasks') !== null) {
-            tasks = JSON.parse(localStorage.getItem('tasks'));
-        }
+		return tasks;
+	};
 
-        return tasks;
-    }
+	showTasks = () => {
+		// get the updated tasks, set the state to be able to loop throught them
+		let tasks = this.getTasksFromLocalStorage();
+		this.setState({ tasks: tasks });
+	};
 
-    showTasks = () => {
-        // get the updated tasks, set the state to be able to loop throught them
-        let tasks = this.getTasksFromLocalStorage();
-        this.setState({tasks: tasks});
-    }
+	addTaskHandler = (e, showTasks) => {
+		e.preventDefault();
 
-    addTaskHandler = (e, showTasks) => {
-        e.preventDefault()
+		// create a task object
+		let task = { id: Math.random() * 511512, title: this.state.task, done: false };
 
-        // create a task object
-        let task = {id: Math.random() * 511512 , title: this.state.task}
-        
+		// get tasks arr, push the new task, set the tasks again in localStorage
 
-        // get tasks arr, push the new task, set the tasks again in localStorage
+		let tasks = this.getTasksFromLocalStorage();
 
-        let tasks = this.getTasksFromLocalStorage();
+		// update the tasks immutablly
+		let updatedTasks = [ ...tasks ];
 
-    
-        // update the tasks immutablly
-        let updatedTasks = [...tasks]
+		updatedTasks.push(task);
 
-        updatedTasks.push(task);
+		localStorage.setItem('tasks', JSON.stringify(updatedTasks));
 
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+		// show tasks as a callback
+		showTasks();
+	};
 
-        // show tasks as a callback
-        showTasks();
-    }
+	getSingleTask = taskId => {
+		let tasks = this.getTasksFromLocalStorage();
 
-    getSingleTask = (taskId) => {
-        let tasks = this.getTasksFromLocalStorage();
+		let targetTask = tasks.find(task => task.id === taskId);
 
-        let targetTask = tasks.find(task => task.id === taskId);
+		this.setState({ targetTask: targetTask });
+	};
 
-        this.setState({targetTask: targetTask})
-    }
+	editTask = (newTaskText, showTasks) => {
+		// get the targeted task ID from the state as i save it when the user click on a certain edit icon
+		let taskId = this.state.targetTask.id;
 
+		// get all tasks
+		let tasks = this.getTasksFromLocalStorage();
 
-    editTask = (newTaskText, showTasks) => {
-        // get the targeted task ID from the state as i save it when the user click on a certain edit icon
-        let taskId = this.state.targetTask.id;
+		// get them immutablly
+		let updatedTasks = [ ...tasks ];
 
-        // get all tasks
-        let tasks = this.getTasksFromLocalStorage();
-        
-        // get them immutablly
-        let updatedTasks = [...tasks];
-        
-        // get the targetTaskIndex to be able to replace it in the array with index
-        let taskIndex = updatedTasks.findIndex(task => task.id === taskId);
+		// get the targetTaskIndex to be able to replace it in the array with index
+		let taskIndex = updatedTasks.findIndex(task => task.id === taskId);
 
-        // get the target task using the found index
-        let targetedTask = updatedTasks[taskIndex];
+		// get the target task using the found index
+		let targetedTask = updatedTasks[taskIndex];
 
-        // get the target immutablly
-        let updatedTask = {...targetedTask};
+		// get the target immutablly
+		let updatedTask = { ...targetedTask };
 
-        // update the target object
-        updatedTask.title = newTaskText;
+		// update the target object
+		updatedTask.title = newTaskText;
 
-        // replace the oldTask in updatedTasks with the updated one
-        updatedTasks[taskIndex] = updatedTask;
+		// replace the oldTask in updatedTasks with the updated one
+		updatedTasks[taskIndex] = updatedTask;
 
-        // replace old tasks with the updatedTasks
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+		// replace old tasks with the updatedTasks
+		localStorage.setItem('tasks', JSON.stringify(updatedTasks));
 
-        // show tasks as a callback
-        showTasks();
-    }
+		// show tasks as a callback
+		showTasks();
+	};
 
-    deleteTask = (taskId, showTasks) => {
+	deleteTask = (taskId, showTasks) => {
+		console.log(taskId, showTasks);
 
-        console.log(taskId, showTasks);
-        
-        // get tasks
-        let tasks = this.getTasksFromLocalStorage();
+		// get tasks
+		let tasks = this.getTasksFromLocalStorage();
 
-        let updatedTasks = [...tasks];
+		let updatedTasks = [ ...tasks ];
 
-        let remainedTasks = updatedTasks.filter(task => task.id !== taskId);
+		let remainedTasks = updatedTasks.filter(task => task.id !== taskId);
 
-        localStorage.setItem('tasks', JSON.stringify(remainedTasks));
+		localStorage.setItem('tasks', JSON.stringify(remainedTasks));
 
-        showTasks();
-    }
+		showTasks();
+	};
 
-    openModal = () => {
-        this.setState({modalOpened: true});
-    }
+	openModal = () => {
+		this.setState({ modalOpened: true });
+	};
 
-    closeModal = () => {
-        this.setState({modalOpened: false});
-    }
+	closeModal = () => {
+		this.setState({ modalOpened: false });
+	};
 
-    componentDidMount(){
-        // get the tasks arr from localStorage and display them for the first time the user open the app
-        this.showTasks()
-    }
+	componentDidMount() {
+		// get the tasks arr from localStorage and display them for the first time the user open the app
+		this.showTasks();
+	}
 
-    render() {
-        return (
-            <div id='ToDoApp'>
-                {/* title */}
-                <h1 className='title'>Add Task</h1>
+	render() {
+		return (
+			<div id='ToDoApp'>
+				{/* title */}
+				<h1 className='title'>Add Task</h1>
 
-                {/* input part */}
-                <form className='form' onSubmit = {(e) => this.addTaskHandler(e, this.showTasks)}>
-                    <input type="text" className='formInput' required onChange={this.writeTaskHandler}/>
-                    <button type='submit' className='formButton'>Add</button>  
-                </form>
+				{/* input part */}
+				<form className='form' onSubmit={e => this.addTaskHandler(e, this.showTasks)}>
+					<input type='text' className='formInput' required onChange={this.writeTaskHandler} />
+					<button type='submit' className='formButton'>
+						Add
+					</button>
+				</form>
 
-                {/* todos Section */}
-                <Tasks 
-                tasks = {this.state.tasks}
-                deleteTask = {this.deleteTask}
-                showTasks ={this.showTasks}
-                openModal ={this.openModal}
-                getSingleTask = {this.getSingleTask}
-                />
+				{/* todos Section */}
+				<Tasks
+					tasks={this.state.tasks}
+					deleteTask={this.deleteTask}
+					showTasks={this.showTasks}
+					openModal={this.openModal}
+					getSingleTask={this.getSingleTask}
+				/>
 
-                {/* modal */}
-                {this.state.modalOpened && <Modal 
-                closeModal = {this.closeModal}
-                targetTask = {this.state.targetTask}
-                showTasks = {this.showTasks}
-                editTask = {this.editTask}
-                
-                />}
-            </div>
-        )
-    }
+				{/* modal */}
+				{this.state.modalOpened && (
+					<Modal
+						closeModal={this.closeModal}
+						targetTask={this.state.targetTask}
+						showTasks={this.showTasks}
+						editTask={this.editTask}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default ToDo;
